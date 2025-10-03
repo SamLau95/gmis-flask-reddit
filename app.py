09e053d92282d953d110f7388769e34f42385b01
Sam Lau <samlau95@gmail.com>
2025-10-02 19:31:54 -0700

    homepage

--- app.py
+++ app.py
@@ -1,14 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+dog_links = [
+    {
+        "title": "30 Fun and Fascinating Dog Facts",
+        "url": "https://www.akc.org/expert-advice/lifestyle/dog-facts/",
+        "score": 10,
+    },
+    {
+        "title": "Why Do Dogs Tilt Their Heads?",
+        "url": "https://www.sciencefocus.com/nature/why-do-dogs-tilt-their-head-when-you-speak-to-them",
+        "score": 5,
+    },
+    {
+        "title": "r/dogs — top posts",
+        "url": "https://www.reddit.com/r/dogs/",
+        "score": 3,
+    },
+    {
+        "title": "Basic Dog Training Guide",
+        "url": "https://www.animalhumanesociety.org/resource/how-get-most-out-training-your-dog",
+        "score": 2,
+    },
+    {
+        "title": "The Dogist (photo stories)",
+        "url": "https://thedogist.com/",
+        "score": 1,
+    },
+]
+
 
 @app.get("/")
-def hello_world():
-    return "Hello, World!"
-
-
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
-
-
+def homepage():
+    return render_template("index.html", links=dog_links)
